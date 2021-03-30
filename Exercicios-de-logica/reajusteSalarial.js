ad6ffa29e561ee8880385d16adbff26952322e6c
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-03-30 15:46:56 -0300

    Códigos

--- Exercicios-de-logica/reajusteSalarial.js
+++ Exercicios-de-logica/reajusteSalarial.js
@@ -1,5 +1,25 @@
 function calcReajusteSalarial(lista, funcNome){
-    let result = lista.reduce(e)
+    let result = lista.filter(e => e.nome == funcNome).map(reajuste);
+
+    return result.toString();
+};
+
+const reajuste = e => {
+    if(e.salario <= 1100){
+        let salarioComReajuste = e.salario + (e.salario * 20/100);
+
+        return `O funcionário ${e.nome} teve um reajuste de 20% no salário: salário ${e.salario.toFixed(2).toString().replace(".", ",")} | salário com Reajuste ${salarioComReajuste.toFixed(2).toString().replace(".", ",")}.`;
+
+    }else if(e.salario > 1100 && e.salario <= 1500){
+        let salarioComReajuste = e.salario + (e.salario * 10/100);
+
+        return `O funcionário ${e.nome} teve um reajuste de 10% no salário: salário ${e.salario.toFixed(2).toString().replace(".", ",")} | salário com Reajuste ${salarioComReajuste.toFixed(2).toString().replace(".", ",")}.`;
+
+    }else{
+        let salarioComReajuste = e.salario + (e.salario * 5/100);
+
+        return `O funcionário ${e.nome} teve um reajuste de 5% no salário: salário ${e.salario.toFixed(2).toString().replace(".", ",")} | salário com Reajuste R$${salarioComReajuste.toFixed(2).toString().replace(".", ",")}.`;
+    };
 };
 
 const funcionarios = [
@@ -7,5 +27,7 @@ const funcionarios = [
     {nome: 'Carlos', funcao: 'Administração', salario: 1462.00},
     {nome: 'Janaina', funcao: 'Reposição', salario: 1462.00},
     {nome: 'Gabriel', funcao: 'Assistente-RH', salario: 1325.20},
-    {nome: 'Ana', funcao: 'Assistente-ADM', salario: 1205.36}
+    {nome: 'Carina', funcao: 'Assistente-ADM', salario: 1205.36}
 ];
+
+console.log(calcReajusteSalarial(funcionarios, 'Carina'));
