85d8f6075edd417d5b58489544e854b0a4bbd9c4
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-08-06 19:46:09 -0300

    Update clacularMedia.js

--- Exercicios-de-logica/funções/clacularMedia.js
+++ Exercicios-de-logica/funções/clacularMedia.js
@@ -1,10 +1,27 @@
 function calcularMedia(notas, tipo) {
-    let mediaFinal ;
-    let tipo = tipo.toUppercase();
+    let tipoMedia = tipo.toUpperCase();
 
-    if(tipo === 'A'){
-        mediaFinal = (notas.reduce((e, a) => e + a)) / notas.lenght;
-    }else if(tipo === 'P'){
-        
+    if(tipoMedia == 'A'){
+        mediaFinal = notas.reduce(mediaAritmetica) / notas.length;
+    }else if(tipoMedia == 'P'){
+        mediaFinal = notas.map(mediaPonderada).reduce((a, e) => a + e) / (5 + 3 +2);
     }
+
+    return `A média final do aluno é igual á: ${mediaFinal.toFixed(1) - 0.1}. `;
 }
+
+const mediaAritmetica = (e, a) => e + a;
+
+const mediaPonderada = (e, i) => {
+    if(i == 0){
+        return e + 5;
+    }else if(i == 2){
+        return e + 3;
+    }else {
+        return e + 2;
+    };
+}
+
+const notas = [5.3, 5.9, 8.3];
+
+console.log(calcularMedia(notas, 'p'));
